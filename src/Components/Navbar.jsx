fe3eb1f4c3ff41a695e12db15b0062707af4794b
AlgoWebBot <shimulzahan636@gmail.com>
2023-12-21 02:25:09 +0600

    something is update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -29,13 +29,13 @@ const Navbar = () => {
             <div className='block lg:flex justify-between items-center space-y-3 relative'>
                 <div className='flex justify-between'>
                     <h1 className='text-xl font-bold px-4'>Logo</h1>
-                    <h1 onClick={()=> setShowMenu(!showMenu)} className='text-xl font-bold px-4 block lg:hidden'>X</h1>
+                    <h1 onClick={()=> setShowMenu(!showMenu)} className='text-xl font-bold px-4 block lg:hidden cursor-pointer'>X</h1>
                 </div>
                 <div className='hidden list-none lg:flex justify-center items-center gap-10'>
                     {
                         navitem?.map(item =>
                             <li>
-                                <Link to={item.link}>{item.navMenu}</Link>
+                                <Link className='capitalize' to={item.link}>{item.navMenu}</Link>
                             </li>
                         )
                     }
@@ -43,11 +43,11 @@ const Navbar = () => {
                         <Link className='bg-red-500 px-6 py-2 rounded-full'>Login</Link>
                     </button>
                 </div>
-                <div className={`absolute px-4 lg:hidden duration-700 list-none space-y-3 w-full bg-green-500 py-6 ${showMenu? 'left-0':'left-[-100%]'}`}>
+                <div className={`absolute px-4 lg:hidden duration-700 top-10 list-none space-y-5 w-full bg-green-500 py-6 ${showMenu? 'left-0':'left-[-100%]'}`}>
                     {
                         navitem?.map(item =>
                             <li>
-                                <Link to={item.link}>{item.navMenu}</Link>
+                                <Link className='capitalize' to={item.link}>{item.navMenu}</Link>
                             </li>
                         )
                     }
